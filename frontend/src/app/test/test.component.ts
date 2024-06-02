4ac6bd0262f457d32b8666e0669317a4c00dbd33
Sudhir Patil <sudhir7patil7@yahoo.com>
2024-06-02 17:10:31 +0530

    15----

---
+++ frontend/src/app/test/test.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import * as alertyfy from 'alertifyjs';
+
+@Component({
+  selector: 'app-test',
+  templateUrl: './test.component.html',
+  styleUrls: ['./test.component.css']
+})
+export class TestComponent {
+
+}
